6e49af7f4aa29cc911a0c2dc974a092659c2dc60
Kotecha Udit Hitendra <64374551+kirito-udit@users.noreply.github.com>
2021-04-09 21:16:51 +0530

    Create PaypalConfig.java

---
+++ PaypalConfig.java
@@ -0,0 +1,15 @@
+public class PaypalConfig {
+
+    @Value("${paypal.client.id}")
+    private String clientId;
+    @Value("${paypal.client.secret}")
+    private String clientSecret;
+    @Value("${paypal.mode}")
+    private String mode;
+
+    @Bean
+    public Map<String, String> paypalSdkConfig() {
+        Map<String, String> configMap = new HashMap<>();
+        configMap.put("mode", mode);
+        return configMap;
+    }
