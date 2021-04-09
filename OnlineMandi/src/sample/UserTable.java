bd15d60ac6844e3411600ee5c7f3b288a262f613
Kartikeya Srivastava <60413404+kartikeyasri23@users.noreply.github.com>
2021-04-10 00:40:56 +0530

    Update UserTable.java

--- OnlineMandi/src/sample/UserTable.java
+++ OnlineMandi/src/sample/UserTable.java
@@ -1,5 +1,7 @@
 package sample;
 import javafx.scene.image.Image;
+
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.*;
@@ -26,6 +28,9 @@ public class UserTable {
     public static final String UPDATE_DOB = "UPDATE " + UTABLE + " SET " + COLUMN_DOB + " = ? WHERE " + COLUMN_ID + " = ?";
     public static final String UPDATE_PASSWORD = "UPDATE " + UTABLE + " SET " + COLUMN_PASSWORD + " = ? WHERE " + COLUMN_ID + " = ?";
     public static final String UPDATE_PROFILE_PIC = "UPDATE " + UTABLE + " SET " + COLUMN_PROFILE_PIC + " = ? WHERE " + COLUMN_ID + " = ?";
+    public static final String QUERY_PASSWORD_VERIFICATION=" SELECT "+COLUMN_PASSWORD+" FROM "+UTABLE+" WHERE "+COLUMN_PHONE+" = ? ";
+    public static final String SELECT_FULL_NAME_WITH_THIS_PHONE_NUMBER="SELECT "+COLUMN_NAME+" FROM "+UTABLE+" WHERE "+COLUMN_PHONE+" = ? ";
+    public static final String SELECT_PROFILE_PIC_WITH_THIS_PHONE_NUMBER="SELECT "+COLUMN_PROFILE_PIC+" FROM "+UTABLE+" WHERE "+COLUMN_PHONE+" ? ";
 
     public Connection conn;
     public PreparedStatement insertUser;
@@ -35,6 +40,9 @@ public class UserTable {
     public PreparedStatement updateCity;
     public PreparedStatement updatePassword;
     public PreparedStatement updateProfilePic;
+    public PreparedStatement queryPasswordVerfication;
+    public PreparedStatement selectFullNameWithThisPhoneNumber;
+    public PreparedStatement selectProfilePicWithThisPhoneNumber;
 
     private static UserTable userTable;
 
@@ -58,6 +66,9 @@ public class UserTable {
             updateCity = conn.prepareStatement(UPDATE_CITY);
             updatePassword = conn.prepareStatement(UPDATE_PASSWORD);
             updateProfilePic = conn.prepareStatement(UPDATE_PROFILE_PIC);
+            queryPasswordVerfication=conn.prepareStatement(QUERY_PASSWORD_VERIFICATION);
+            selectFullNameWithThisPhoneNumber=conn.prepareStatement(SELECT_FULL_NAME_WITH_THIS_PHONE_NUMBER);
+            selectProfilePicWithThisPhoneNumber=conn.prepareStatement(SELECT_PROFILE_PIC_WITH_THIS_PHONE_NUMBER);
             return true;
         } catch (SQLException e) {
             System.out.println("Open failed!" + e.getMessage());
@@ -90,6 +101,15 @@ public class UserTable {
             }
             if (conn != null) {
                 conn.close();
+            }
+            if(queryPasswordVerfication!=null){
+                queryPasswordVerfication.close();
+            }
+            if(selectProfilePicWithThisPhoneNumber!=null){
+                selectProfilePicWithThisPhoneNumber.close();
+            }
+            if(selectFullNameWithThisPhoneNumber!=null){
+                selectFullNameWithThisPhoneNumber.close();
             }
         } catch (SQLException e) {
             System.out.println("Couldn't close connections: " + e.getMessage());
@@ -156,6 +176,52 @@ public class UserTable {
             updateProfilePic.executeUpdate();
         } catch (Exception e) {
             System.out.println("Update ProfilePic failed: " + e.getMessage());
+        }
+    }
+
+    public FullNameProfilePic authentication(String password,String phoneNumber){
+        try {
+            queryPasswordVerfication.setString(1,phoneNumber);
+            ResultSet results=queryPasswordVerfication.executeQuery();
+            while(results.next()){
+                if(results.getString(1)==password) {
+                    FullNameProfilePic fullNameProfilePic=new FullNameProfilePic(getFullName(phoneNumber), getProfilePic(phoneNumber));
+                    return fullNameProfilePic;
+                }
+            }
+            return null;
+        }catch(Exception e){
+            System.out.println("Exception occured while password verifcation during user login "+e.getMessage());
+            return null;
+        }
+    }
+
+    private String getFullName(String phoneNumber){
+        try{
+            selectFullNameWithThisPhoneNumber.setString(1,phoneNumber);
+            ResultSet results=selectFullNameWithThisPhoneNumber.executeQuery();
+            while(results.next()){
+                return results.getString(1);
+            }
+            return null;
+        }catch (Exception e){
+            System.out.println("Exception occured while checking the full name of the user during user login "+e.getMessage());
+            return null;
+        }
+    }
+    private Image getProfilePic(String phoneNumber){
+        try{
+            selectProfilePicWithThisPhoneNumber.setString(1,phoneNumber);
+            ResultSet results=selectProfilePicWithThisPhoneNumber.executeQuery();
+            while(results.next()){
+                FileInputStream fis= (FileInputStream) results.getBinaryStream(1);
+                Image image=new Image(fis);
+                return image;
+            }
+            return null;
+        }catch (Exception e){
+            System.out.println("Exception occured while checking the profile pic of the user during user login "+e.getMessage());
+            return null;
         }
     }
 }
