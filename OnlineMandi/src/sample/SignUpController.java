4a6ab25af6599c65c103cfeea621958033b30969
Kartikeya Srivastava <60413404+kartikeyasri23@users.noreply.github.com>
2021-04-09 23:57:04 +0530

    Updated SignUpController.java

--- OnlineMandi/src/sample/SignUpController.java
+++ OnlineMandi/src/sample/SignUpController.java
@@ -1,21 +1,35 @@
 package sample;
+
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
+
 import java.net.URL;
 import java.util.ResourceBundle;
+
 public class SignUpController implements Initializable {
     @FXML
     private GridPane gridPane;
     @FXML
     private BorderPane borderPane;
+ public java.lang.String getname()
+    {
+        java.lang.String x = "k";
+        return x;
+    }
+    public java.lang.String getpass()
+    {
+        return "t";
+    }
+    public java.lang.String getmail()
+    {
+        return "t";
+    }
     @Override
     public void initialize(URL url, ResourceBundle rb) {
     Image image = new Image("file:///C:/Users/hp/Desktop/farm.jpg");
-        BackgroundSize bSize = new BackgroundSize(BackgroundSize.AUTO, BackgroundSize.AUTO, false, false, true, false);
-        Background background = new Background(new BackgroundImage(image,
     BackgroundSize bSize = new BackgroundSize(BackgroundSize.AUTO, BackgroundSize.AUTO, false, false, true, false);
     Background background = new Background(new BackgroundImage(image,
                 BackgroundRepeat.NO_REPEAT,
@@ -23,7 +37,6 @@ public class SignUpController implements Initializable {
                 BackgroundPosition.CENTER,
                 bSize));
 
-        borderPane.setBackground(background);
     borderPane.setBackground(background);
     }
 }
