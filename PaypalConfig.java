47d39ceba8ae1569b58933667b82f79a6b0a5c37
Kotecha Udit Hitendra <64374551+kirito-udit@users.noreply.github.com>
2021-04-09 21:17:15 +0530

    Update PaypalConfig.java

--- PaypalConfig.java
+++ PaypalConfig.java
@@ -1,3 +1,18 @@
+package com.om.onlinemandi01;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.paypal.api.payments.PaymentHistory;
+import com.paypal.base.rest.APIContext;
+import com.paypal.base.rest.OAuthTokenCredential;
+import com.paypal.base.rest.PayPalRESTException;
+
+@Configuration
 public class PaypalConfig {
 
     @Value("${paypal.client.id}")
@@ -13,3 +28,17 @@ public class PaypalConfig {
         configMap.put("mode", mode);
         return configMap;
     }
+
+    @Bean
+    public OAuthTokenCredential oAuthTokenCredential() {
+        return new OAuthTokenCredential(clientId, clientSecret, paypalSdkConfig());
+    }
+
+    @Bean
+    public APIContext apiContext() throws PayPalRESTException {
+        APIContext context = new APIContext(oAuthTokenCredential().getAccessToken());
+        context.setConfigurationMap(paypalSdkConfig());
+        return context;
+    }
+
+}
