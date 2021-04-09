32e53cdbf20b2ca8fbffd9ce0552fa63c672245d
Kartikeya Srivastava <60413404+kartikeyasri23@users.noreply.github.com>
2021-04-10 00:54:13 +0530

    Update ProfilePageController.java

--- OnlineMandi/src/sample/ProfilePageController.java
+++ OnlineMandi/src/sample/ProfilePageController.java
@@ -1,13 +1,18 @@
 package sample;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 
 import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ProfilePageController implements Initializable {
     @FXML
     private BorderPane profilePane;
 
@@ -17,11 +22,16 @@ import java.net.URL;
     @FXML
     private Label nameLabel;
 
+    @FXML
+    private Button refreshButton;
+    private Image img;
 
     void createProfile(Image image, String name)
     {
-        profilePic.setImage(image);
         nameLabel.setText(name);
+        this.img = image;
+        profilePic.setImage(image);
+        profilePic.setVisible(true);
     }
 
     @Override
@@ -34,5 +44,12 @@ import java.net.URL;
                 BackgroundPosition.CENTER,
                 bSize));
         profilePane.setBackground(background);
+    }
+
+    @FXML
+    public void refresh() {
+        profilePic.setImage(img);
+        profilePic.setVisible(true);
+        System.out.println("BUMTON PREMSED");
     }
 }
