1179ff2466233a52dae4ad4a5affc9f9e62ad787
Kartikeya Srivastava <60413404+kartikeyasri23@users.noreply.github.com>
2021-04-10 01:18:09 +0530

    Update SellPageController.java

--- OnlineMandi/src/sample/SellPageController.java
+++ OnlineMandi/src/sample/SellPageController.java
@@ -1,4 +1,55 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+
+import java.io.IOException;
+
 public class SellPageController {
+    @FXML
+    private Button addOffersButton;
+
+    @FXML
+    private Button viewOffersButton;
+    private String phoneNo;
+    private String name;
+
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @FXML
+    void addOffer(ActionEvent e) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("AddOfferPage.fxml"));
+        Parent root = (Parent) loader.load();
+        SellPageController  aopc = loader.getController();
+        aopc.setName(name);
+        aopc.setPhoneNo(phoneNo);
+        Scene scene = new Scene(root, 580, 790);
+        Main.primaryStage.setTitle("Login");
+        Main.primaryStage.setScene(scene);
+        Main.primaryStage.show();
+    }
+
+    @FXML
+    void viewOffer(ActionEvent e){
+
+    }
+
 }
