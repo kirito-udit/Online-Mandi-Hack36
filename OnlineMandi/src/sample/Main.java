af4720ed7cc3800560971de822e06f9c724786c0
shubhamdixit760 <66271037+shubhamdixit760@users.noreply.github.com>
2021-04-10 01:08:06 +0530

    Added sockets in main class

--- OnlineMandi/src/sample/Main.java
+++ OnlineMandi/src/sample/Main.java
@@ -1,5 +1,4 @@
 package sample;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -18,7 +17,21 @@ public class Main extends Application {
     public void start(Stage primary) throws Exception{
         userTable = UserTable.getInstance();
         userTable.open();
+//        //Sign Up Page
+//        Parent root = FXMLLoader.load(getClass().getResource("SignUpForm.fxml"));
+//        primaryStage = primary;
+//        primaryStage.setTitle("Sign up");
+//        primaryStage.setScene(new Scene(root, 1023, 614));
+//        primaryStage.show();
 
+          //Profile Page
+//        Parent root = FXMLLoader.load(getClass().getResource("ProfilePage.fxml"));
+//        primaryStage = primary;
+//        primaryStage.setTitle("Profile");
+//        primaryStage.setScene(new Scene(root, 900, 600));
+//        primaryStage.show();
+
+            //Start Page
         //Start Page
         Parent root = FXMLLoader.load(getClass().getResource("Start.fxml"));
         primaryStage = primary;
@@ -26,6 +39,7 @@ public class Main extends Application {
         primaryStage.setScene(new Scene(root, 720, 540));
         primaryStage.show();
     }
+    public static void main(String[] args) {
     public static void main(String[] args) throws IOException {
         socket = new Socket("localhost",6963);
         launch(args);
