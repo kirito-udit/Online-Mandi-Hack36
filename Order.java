2269bc6679cfca6961fb3cdf326e6548ca02ef70
Kotecha Udit Hitendra <64374551+kirito-udit@users.noreply.github.com>
2021-04-09 21:15:11 +0530

    Create Order.java

---
+++ Order.java
@@ -0,0 +1,17 @@
+package com.om.onlinemandi01;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.ui.Model;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Order {
+
+   
+    Model model;
+}
