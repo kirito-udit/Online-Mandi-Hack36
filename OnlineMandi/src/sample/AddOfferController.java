06ced96ad1e26ca166b3b33da4cef009eca69221
Kartikeya Srivastava <60413404+kartikeyasri23@users.noreply.github.com>
2021-04-10 01:20:14 +0530

    Update AddOfferController.java

--- OnlineMandi/src/sample/AddOfferController.java
+++ OnlineMandi/src/sample/AddOfferController.java
@@ -1,4 +1,64 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+import java.sql.Date;
+import java.time.LocalDate;
+
 public class AddOfferController {
+    @FXML
+    private TextField cropTextField;
+
+    @FXML
+    private TextArea descriptionTextArea;
+
+    @FXML
+    private DatePicker startDate;
+
+    @FXML
+    private DatePicker endDate;
+
+    @FXML
+    private Spinner quantitySpinner;
+
+    @FXML
+    private TextField priceTextField;
+
+    private String name,phoneNo;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    @FXML
+    void addOffer(ActionEvent e){
+        String cropName = cropTextField.getText();
+        String desc = descriptionTextArea.getText();
+        java.sql.Date startDateValue = Date.valueOf(startDate.getValue());
+        java.sql.Date endDateValue = Date.valueOf(endDate.getValue());
+        int quantity = (int) quantitySpinner.getValue();
+        String price = priceTextField.getText();
+
+        SellerTable sellerTable = SellerTable.getInstance();
+        sellerTable.addOffer(cropName,quantity,price,startDateValue,endDateValue,name,phoneNo);
+
+    }
+
 }
