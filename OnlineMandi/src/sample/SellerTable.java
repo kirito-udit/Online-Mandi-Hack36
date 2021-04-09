82de73b6a439e7cf47303b0faf5de40c111317eb
shubhamdixit760 <66271037+shubhamdixit760@users.noreply.github.com>
2021-04-10 01:22:57 +0530

    Update SellerTable.java

--- OnlineMandi/src/sample/SellerTable.java
+++ OnlineMandi/src/sample/SellerTable.java
@@ -3,6 +3,25 @@ package sample;
 import java.sql.*;
 
 public class SellerTable {
+    public static final String DB_NAME="SELLER.DB";
+    public static final String CONNECTION_STRING = "jdbc:sqlite:C:\\Users\\Kartikeya"+DB_NAME;
+    public static final String SELL_TABLE="SellTable";
+    public static final String COLUMN_SELLER_PHONE="SellerPhone";
+    public static final String COLUMN_SELLER_NAME="SellerName";
+    public static final String COLUMN_START_DATE="StartDate";
+    public static final String COLUMN_CROP_NAME="CropName";
+    public static final String COLUMN_QUANTITY="Quantity";
+    public static final String COLUMN_END_DATE="EndDate";
+    public static final String COLUMN_OFFER_ID="OfferId";
+    public static final String COLUMN_PRICE="Price";
+    public static final String ADD_OFFER=" INSERT INTO "+SELL_TABLE+" VALUES ( ? , ? , ? , ? , ? , ? , ? , ? ) ";
+    public static final String UPDATE_OFFER_PRICE=" UPDATE "+SELL_TABLE+" SET "+COLUMN_PRICE+" = ? WHERE "+
+            COLUMN_OFFER_ID+" = ? ";
+    public static final String UPDATE_OFFER_END_DATE=" UPDATE "+SELL_TABLE+" SET "+COLUMN_END_DATE+" = ? "+" WHERE "+
+            COLUMN_OFFER_ID+" = ? ";
+    public static final String UPDATE_OFFER_QUANTITY=" UPDATE "+SELL_TABLE+" SET "+COLUMN_QUANTITY+" = ? "+" WHERE "+
+            COLUMN_OFFER_ID+" = ? ";
+    public static final String DELETE_OFFER=" DELETE FROM "+SELL_TABLE+" WHERE "+COLUMN_OFFER_ID+" = ? ";
     public static final String DB_NAME = "SELLER.db";
     public static final String CONNECTION_STRING = "jdbc:sqlite:D:\\databases\\" + DB_NAME;
     public static final String SELL_TABLE = "SellTable";
@@ -14,7 +33,8 @@ public class SellerTable {
     public static final String COLUMN_END_DATE = "EndDate";
     public static final String COLUMN_OFFER_ID = "OfferId";
     public static final String COLUMN_PRICE = "Price";
-    public static final String ADD_OFFER = " INSERT INTO " + SELL_TABLE + " VALUES ( ? , ? , ? , ? , ? , ? , ? , ? ) ";
+
+    public static final String ADD_OFFER = " INSERT INTO " + SELL_TABLE + " VALUES ( NULL, ? , ? , ? , ? , ? , ? , ? ) ";
     public static final String UPDATE_OFFER_PRICE = " UPDATE " + SELL_TABLE + " SET " + COLUMN_PRICE + " = ? WHERE " +
             COLUMN_OFFER_ID + " = ? ";
     public static final String UPDATE_OFFER_END_DATE = " UPDATE " + SELL_TABLE + " SET " + COLUMN_END_DATE + " = ? " + " WHERE " +
@@ -29,6 +49,14 @@ public class SellerTable {
     public PreparedStatement updateOfferEndDate;
     public PreparedStatement updateOfferQuantity;
     public PreparedStatement deleteOffer;
+    public boolean open(){
+        try{
+            conn= DriverManager.getConnection(CONNECTION_STRING);
+            addOffer=conn.prepareStatement(ADD_OFFER);
+            updateOfferPrice=conn.prepareStatement(UPDATE_OFFER_PRICE);
+            updateOfferEndDate=conn.prepareStatement(UPDATE_OFFER_END_DATE);
+            updateOfferQuantity=conn.prepareStatement(UPDATE_OFFER_QUANTITY);
+            deleteOffer=conn.prepareStatement(DELETE_OFFER);
 
     private static SellerTable sellerTable;
 
@@ -44,7 +72,7 @@ public class SellerTable {
 
     public boolean open() {
         try {
-            conn = DriverManager.getConnection(CONNECTION_STRING);
+            conn = Server.conn;
             addOffer = conn.prepareStatement(ADD_OFFER);
             updateOfferPrice = conn.prepareStatement(UPDATE_OFFER_PRICE);
             updateOfferEndDate = conn.prepareStatement(UPDATE_OFFER_END_DATE);
@@ -52,50 +80,63 @@ public class SellerTable {
             deleteOffer = conn.prepareStatement(DELETE_OFFER);
             return true;
         } catch (SQLException e) {
+            System.out.println("Error occured while opening the seller database "+e.getMessage());
             System.out.println("Error occured while opening the seller database " + e.getMessage());
             return false;
         }
     }
+    public boolean close(){
+        try{
+            if(conn!=null){
+                conn.close();
+            }if(addOffer!=null){
 
     public boolean close() {
         try {
-            if (conn != null) {
-                conn.close();
-            }
             if (addOffer != null) {
                 addOffer.close();
+            }if(deleteOffer!=null){
             }
             if (deleteOffer != null) {
                 deleteOffer.close();
+            }if(updateOfferQuantity!=null){
             }
             if (updateOfferQuantity != null) {
                 updateOfferQuantity.close();
+            }if(updateOfferPrice!=null){
             }
             if (updateOfferPrice != null) {
                 updateOfferPrice.close();
+            }if(updateOfferEndDate!=null){
             }
             if (updateOfferEndDate != null) {
                 updateOfferEndDate.close();
             }
             return true;
         } catch (SQLException e) {
+            System.out.println("Error occured while closing the resources of the seller database "+e.getMessage());
             System.out.println("Error occured while closing the resources of the seller database " + e.getMessage());
             return false;
         }
     }
+    private boolean deleteOffer(int offerId){
 
     public boolean deleteOffer(int offerId) {
         try {
             deleteOffer.setInt(1, offerId);
             deleteOffer.executeQuery();
             return true;
+        }catch(SQLException e){
+            System.out.println("Error occured while deleting the offer "+e.getMessage());
         } catch (SQLException e) {
             System.out.println("Error occured while deleting the offer " + e.getMessage());
             return false;
         }
     }
+    private boolean addOffer(int offerId,String cropName,int quantity,int price,
+                             Date startDate,Date endDate,String sellerName,String sellerPhone) {
 
-    public boolean addOffer(int offerId, String cropName, int quantity, int price,
+    public boolean addOffer(String cropName, int quantity, int price,
                              Date startDate, Date endDate, String sellerName, String sellerPhone) {
         try {
             addOffer.setInt(1, offerId);
@@ -106,10 +147,60 @@ public class SellerTable {
             addOffer.setDate(6, endDate);
             addOffer.setString(7, sellerName);
             addOffer.setString(8, sellerPhone);
+            addOffer.setString(1, cropName);
+            addOffer.setInt(2, quantity);
+            addOffer.setInt(3, price);
+            addOffer.setDate(4, startDate);
+            addOffer.setDate(5, endDate);
+            addOffer.setString(6, sellerName);
+            addOffer.setString(7, sellerPhone);
             addOffer.executeQuery();
             return true;
         } catch (SQLException e) {
             System.out.println("Error occured while adding an offer to the seller table " + e.getMessage());
+            return false;
+        }
+    }
+
+    public boolean updateOfferPrice(int offerId,int price){
+        try {
+            updateOfferPrice.setInt(1,price);
+            updateOfferPrice.setInt(2,offerId);
+            int affectedRows=updateOfferPrice.executeUpdate();
+            if(affectedRows!=1){
+                throw new SQLException("Couldn't update the offer price");
+            }
+            return true;
+        }catch (SQLException e){
+            System.out.println("Error occured while updating the offer price "+e.getMessage());
+            return false;
+        }
+    }
+    public boolean updateOfferQuantity(int offerId,int quantity){
+        try {
+            updateOfferPrice.setInt(1,quantity);
+            updateOfferPrice.setInt(2,offerId);
+            int affectedRows=updateOfferPrice.executeUpdate();
+            if(affectedRows!=1){
+                throw new SQLException("Couldn't update the offer quantity");
+            }
+            return true;
+        }catch (SQLException e){
+            System.out.println("Error occured while updating the offer quantity "+e.getMessage());
+            return false;
+        }
+    }
+    public boolean updateOfferEndDate(int offerId,Date endDate){
+        try {
+            updateOfferPrice.setDate(1,endDate);
+            updateOfferPrice.setInt(2,offerId);
+            int affectedRows=updateOfferEndDate.executeUpdate();
+            if(affectedRows!=1){
+                throw new SQLException("Couldn't update the offer end date");
+            }
+            return true;
+        }catch (SQLException e){
+            System.out.println("Error occured while updating the end date "+e.getMessage());
             return false;
         }
     }
