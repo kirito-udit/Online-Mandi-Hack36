5b2f54476a8e14ca4c2ae1705de1820707719b87
shubhamdixit760 <66271037+shubhamdixit760@users.noreply.github.com>
2021-04-10 01:04:17 +0530

    Made full name profile pic serializable

--- OnlineMandi/src/sample/FullNameProfilePic.java
+++ OnlineMandi/src/sample/FullNameProfilePic.java
@@ -3,17 +3,18 @@ package sample;
 import javafx.scene.image.Image;
 
 public class FullNameProfilePic {
+import java.io.Serializable;
+
+public class FullNameProfilePic implements Serializable {
     private String fullName;
     private Image image;
     public FullNameProfilePic(String fullName, Image image){
         this.fullName=fullName;
         this.image=image;
     }
-
     public String getFullName() {
         return fullName;
     }
-
     public Image getImage() {
         return image;
     }
