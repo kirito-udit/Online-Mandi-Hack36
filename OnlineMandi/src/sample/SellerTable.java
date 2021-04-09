f78c98f68cd77811e7d3f6f81801c4e88f1426f4
shubhamdixit760 <66271037+shubhamdixit760@users.noreply.github.com>
2021-04-10 01:39:49 +0530

    Update SellerTable.java

--- OnlineMandi/src/sample/SellerTable.java
+++ OnlineMandi/src/sample/SellerTable.java
@@ -1,7 +1,13 @@
 package sample;
+
+import javafx.collections.ObservableList;
+
 import java.sql.*;
+import java.util.ArrayList;
+
 public class SellerTable {
     public static final String DB_NAME = "SELLER.db";
+    public static final String DB_NAME = "register.db";
     public static final String CONNECTION_STRING = "jdbc:sqlite:D:\\databases\\" + DB_NAME;
     public static final String SELL_TABLE = "SellTable";
     public static final String COLUMN_SELLER_PHONE = "SellerPhone";
@@ -20,13 +26,18 @@ public class SellerTable {
     public static final String UPDATE_OFFER_QUANTITY = " UPDATE " + SELL_TABLE + " SET " + COLUMN_QUANTITY + " = ? " + " WHERE " +
             COLUMN_OFFER_ID + " = ? ";
     public static final String DELETE_OFFER = " DELETE FROM " + SELL_TABLE + " WHERE " + COLUMN_OFFER_ID + " = ? ";
+    public static final String GET_ALL_OFFERS = " SELECT * FROM " + SELL_TABLE;
+
     public Connection conn;
     public PreparedStatement addOffer;
     public PreparedStatement updateOfferPrice;
     public PreparedStatement updateOfferEndDate;
     public PreparedStatement updateOfferQuantity;
     public PreparedStatement deleteOffer;
+
+    public PreparedStatement getAllOffers;
     private static SellerTable sellerTable;
+
     public static SellerTable getInstance() {
         if(sellerTable == null)
             sellerTable = new SellerTable();
@@ -34,16 +45,15 @@ public class SellerTable {
     }
     private SellerTable() {
     }
-
     public boolean open() {
         try {
-            conn = Server.conn;
             this.conn = Server.conn;
             addOffer = conn.prepareStatement(ADD_OFFER);
             updateOfferPrice = conn.prepareStatement(UPDATE_OFFER_PRICE);
             updateOfferEndDate = conn.prepareStatement(UPDATE_OFFER_END_DATE);
             updateOfferQuantity = conn.prepareStatement(UPDATE_OFFER_QUANTITY);
             deleteOffer = conn.prepareStatement(DELETE_OFFER);
+            getAllOffers = conn.prepareStatement(GET_ALL_OFFERS);
             return true;
         } catch (SQLException e) {
             System.out.println("Error occured while opening the seller database " + e.getMessage());
@@ -66,6 +76,12 @@ public class SellerTable {
             }
             if (updateOfferEndDate != null) {
                 updateOfferEndDate.close();
+            }
+            if (getAllOffers != null) {
+                getAllOffers.close();
+            }
+            if (conn != null) {
+                conn.close();
             }
             return true;
         } catch (SQLException e) {
@@ -94,6 +110,7 @@ public class SellerTable {
             addOffer.setString(6, sellerName);
             addOffer.setString(7, sellerPhone);
             addOffer.executeQuery();
+            addOffer.executeUpdate();
             return true;
         } catch (SQLException e) {
             System.out.println("Error occured while adding an offer to the seller table " + e.getMessage());
@@ -141,5 +158,34 @@ public class SellerTable {
             System.out.println("Error occured while updating the end date "+e.getMessage());
             return false;
         }
+    }
+
+    public ArrayList<Offer> getAllOffers() {
+        try {
+            ArrayList <Offer> offers = new ArrayList<Offer>();
+            ResultSet resultSet = getAllOffers.executeQuery();
+            while (resultSet.next()) {
+//                System.out.println("1234567");
+//                System.out.println(resultSet.getInt(1));
+//                System.out.println(resultSet.getString(2));
+//                System.out.println(resultSet.getInt(3));
+//                System.out.println(resultSet.getInt(4));
+//                System.out.println(resultSet.getDate(5).toString());
+//                System.out.println(resultSet.getDate(6).toString());
+//                System.out.println(resultSet.getString(7));
+//                System.out.println(resultSet.getString(8));
+
+                Offer offer = new Offer(resultSet.getInt(1),resultSet.getString(2),resultSet.getInt(3),resultSet.getInt(4),resultSet.getDate(5),resultSet.getDate(6),resultSet.getString(7),resultSet.getString(8));
+                System.out.println(offer.getOfferId());
+                System.out.println(offer.getStartDate());
+                System.out.println(offer.getEndDate());
+                offers.add(offer);
+            }
+            return offers;
+        }
+        catch (SQLException e) {
+            System.out.println("Couldn't fetch all offers: "+e.getMessage());
+        }
+        return null;
     }
 }
