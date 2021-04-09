a9f974b815eaf70351399f63aaba360794ba3842
Kartikeya Srivastava <60413404+kartikeyasri23@users.noreply.github.com>
2021-04-10 00:27:02 +0530

    Updated Main.java

--- OnlineMandi/src/sample/Main.java
+++ OnlineMandi/src/sample/Main.java
@@ -7,24 +7,30 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
+    public static Stage primaryStage;
     @Override
-    public void start(Stage primaryStage) throws Exception{
-//        Parent root = FXMLLoader.load(getClass().getResource("SignUpForm.fxml"));
-//        primaryStage.setTitle("Sign up");
-//        primaryStage.setScene(new Scene(root, 1023, 614));
-//        primaryStage.show();
-//        Parent root = FXMLLoader.load(getClass().getResource("LoginForm.fxml"));
-//        primaryStage.setTitle("Login");
-//        primaryStage.setScene(new Scene(root, 580, 790));
-//        primaryStage.show();
-        Parent root = FXMLLoader.load(getClass().getResource("Start.fxml"));
-        primaryStage.setTitle("Welcome to Online Mandi");
-        primaryStage.setScene(new Scene(root, 720, 540));
+    public void start(Stage primary) throws Exception{
+        //Sign Up Page
+        Parent root = FXMLLoader.load(getClass().getResource("SignUpForm.fxml"));
+        primaryStage = primary;
+        primaryStage.setTitle("Sign up");
+        primaryStage.setScene(new Scene(root, 1023, 614));
         primaryStage.show();
+
+          //Profile Page
+//        Parent root = FXMLLoader.load(getClass().getResource("ProfilePage.fxml"));
+//        primaryStage = primary;
+//        primaryStage.setTitle("Profile");
+//        primaryStage.setScene(new Scene(root, 900, 600));
+//        primaryStage.show();
+
+            //Start Page
+//        Parent root = FXMLLoader.load(getClass().getResource("Start.fxml"));
+//        primaryStage = primary;
+//        primaryStage.setTitle("Welcome to Online Mandi!");
+//        primaryStage.setScene(new Scene(root, 720, 540));
+//        primaryStage.show();
     }
-
-
     public static void main(String[] args) {
         launch(args);
     }
