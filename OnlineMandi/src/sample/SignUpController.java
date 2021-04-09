a447bb2db9971a9609c4bed90e28ce026294a76f
shubhamdixit760 <66271037+shubhamdixit760@users.noreply.github.com>
2021-04-10 02:03:50 +0530

    Update SignUpController.java

--- OnlineMandi/src/sample/SignUpController.java
+++ OnlineMandi/src/sample/SignUpController.java
@@ -1,5 +1,4 @@
 package sample;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -14,7 +13,6 @@ import javafx.scene.image.Image;
 import javafx.scene.layout.*;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
-
 import javax.swing.*;
 import java.io.File;
 import java.io.FileInputStream;
@@ -27,43 +25,30 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.util.ResourceBundle;
-
 public class SignUpController implements Initializable {
     @FXML
     private GridPane gridPane;
-
     @FXML
     private BorderPane borderPane;
-
     @FXML
     private Button submit;
-
     @FXML
     private Button upload;
-
     @FXML
     private TextField nameField;
-
     @FXML
     private TextField phoneNumber;
-
     @FXML
     private PasswordField passwordField;
-
     @FXML
     private TextField cityName;
-
     @FXML
     private TextField aadharNumber;
-
     @FXML
     private DatePicker dateOfBirth;
-
     @FXML
     private TextField poBoxNumber;
-
     File selectedFile;
-
     // JOptionPane.showMessageDialog(null,"Message!!!");
     public String getName() {
         String name = nameField.getText();
@@ -79,33 +64,28 @@ public class SignUpController implements Initializable {
         String password = getMd5(passwordField.getText());
         return password;
     }
-
     public String getCity() {
         String city = cityName.getText();
         //here instead of taking input string from textfield we will try a combo box kind of thing
         //but for now return the city
         return city;
     }
-
     public String getAdhar() {
         String aadhar = aadharNumber.getText();
         //verify the correctness of adhaar
         //if not then show joptionpane with a message
         return aadhar;
     }
-
     public java.sql.Date getDob() {
         LocalDate dob = dateOfBirth.getValue();
         java.sql.Date date = Date.valueOf(dob);
         return date;
     }
-
     public String getPIN() {
         String pin = (poBoxNumber.getText());
         //verify correctness of PIN whether it is of six digits or not
         return pin;
     }
-
     @FXML
     void upload(ActionEvent e) {
         FileChooser fc = new FileChooser();
@@ -114,13 +94,10 @@ public class SignUpController implements Initializable {
         //adding filters to the type of file that will be opened
         fc.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("image files","*.png","*.jpg","*.jpeg"));
         selectedFile = fc.showOpenDialog(null);
-
     }
-
     //this function is to be registered with the submit button in scene builder
     @FXML
     void submitResponse(ActionEvent e) throws Exception{
-
         //opens profilePage.fxml on successful register
         String name = getName();
         String phoneNo = getPhone();
@@ -129,7 +106,6 @@ public class SignUpController implements Initializable {
         String aadhar = getAdhar();
         java.sql.Date dob = getDob();
         String poBoxNumber = getPIN();
-
         //fetching the path of the image selectd from fileChooser
         //but before that check if file size exceeds  307200 bytes, if so then error is to be shown
         String local = null;
@@ -137,49 +113,38 @@ public class SignUpController implements Initializable {
 //             JOptionPane.showMessageDialog(null,"File size exceeds 300KB!!!");
 //        else
 //        local = selectedFile.toURI().toString();
-
         //creating the object of singleton class UserTable to avoid creating multiple objects for the same user table entity
         FileInputStream fis = new FileInputStream(selectedFile);
         Main.userTable.insertUser(name,phoneNo,password,city,fis,aadhar,dob,poBoxNumber);
-
         //open a new Scene for Login Page
         Parent root = FXMLLoader.load(getClass().getResource("LoginForm.fxml"));
         Scene scene = new Scene(root, 580, 790);
         Main.primaryStage.setTitle("Login");
         Main.primaryStage.setScene(scene);
         Main.primaryStage.show();
-
-
-
     }
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         Image image = new Image("file:///C:/Users/hp/Desktop/farm.jpg");
+        Image image = new Image("file:./src/sample/Resources/farm.jpg");
         BackgroundSize bSize = new BackgroundSize(BackgroundSize.AUTO, BackgroundSize.AUTO, false, false, true, false);
         Background background = new Background(new BackgroundImage(image,
                 BackgroundRepeat.NO_REPEAT,
                 BackgroundRepeat.NO_REPEAT,
                 BackgroundPosition.CENTER,
                 bSize));
-
         borderPane.setBackground(background);
     }
-
-
     public static String getMd5(String input) {
         try {
-
             // Static getInstance method is called with hashing MD5
             MessageDigest md = MessageDigest.getInstance("MD5");
-
             // digest() method is called to calculate message digest
             //  of an input digest() return array of byte
             byte[] messageDigest = md.digest(input.getBytes());
-
             // Convert byte array into signum representation
             BigInteger no = new BigInteger(1, messageDigest);
-
             // Convert message digest into hex value
             String hashtext = no.toString(16);
             while (hashtext.length() < 32) {
@@ -187,7 +152,6 @@ public class SignUpController implements Initializable {
             }
             return hashtext;
         }
-
         // For specifying wrong message digest algorithms
         catch (NoSuchAlgorithmException e) {
             throw new RuntimeException(e);
