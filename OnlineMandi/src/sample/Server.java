035566a31ea5a3616ade4b18fda77e2448efca18
Saurabh Singh <60233633+mrdinosaurabh@users.noreply.github.com>
2021-04-10 14:42:31 +0530

    Update Server.java

--- OnlineMandi/src/sample/Server.java
+++ OnlineMandi/src/sample/Server.java
@@ -12,60 +12,115 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
+import java.util.Vector;
 
-public class Server {
+public class Server implements  Runnable {
     public static Connection conn=null;
-    public static ArrayList<String> currentlyActiveUser = new ArrayList<>();
-    public static ArrayList <HandleClientRequest> clientHandlers= new ArrayList<>();
+    //public static ArrayList<String> currentlyActiveUser = new ArrayList<>();
+    //public static ArrayList <HandleClientRequest> clientHandlers= new ArrayList<>();
     public static File dbFile = new File("./src/sample/Resources");
     public static final String DB_NAME = "register.db";
     public static final String CONNECTION_STRING = "jdbc:sqlite:"+dbFile.getAbsolutePath()+"\\"+DB_NAME;
-    public static void main(String[] args) {
-        ServerSocket serverSocket = null;
-        Socket socket;
+    private Socket socket;
+    public static ArrayList<ClientDetails>clients = new ArrayList<>();
+
+
+    Server(Socket socket) {
+        this.socket = socket;
+    }
+
+    public void run() {
+        ObjectOutputStream oos = null;
+        ObjectInputStream ois = null;
+        String phoneNumber = null;
+        String password=null;
+        try {
+            oos = new ObjectOutputStream((socket.getOutputStream()));
+            ois = new ObjectInputStream((socket.getInputStream()));
+            phoneNumber = (String)ois.readObject();//this is the first string read by the server
+            password=(String)ois.readObject();
+            FullNameProfilePic fnpc = UserTable.getInstance().authentication(password,phoneNumber);
+            if(fnpc==null) {
+                oos.writeObject("Authentication Failed");
+            }else{
+                oos.writeObject("Authentication Successful");
+                clients.add(new ClientDetails(phoneNumber,oos));
+            }
+        }catch(Exception e){
+            for(ClientDetails client : clients) {
+                if(client.getPhoneNo() == phoneNumber) {
+                    clients.remove(client);
+                    break;
+                }
+            }
+            e.printStackTrace();
+
+            try {
+                socket.close();
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            }
+            return;
+        }
+
+        while(true){
+            String data = null, message=null;
+            Timestamp t = null;
+            try {
+                data = (String) ois.readObject();//this will be the phone No of the reciever whom the sender wants to send msg to
+                t = (Timestamp) ois.readObject();
+                message = (String)ois.readObject();
+                MessageManager mm=MessageManager.getInstance();
+                mm.addConversation(phoneNumber,data, message,t, 0);
+            } catch (Exception e) {
+                e.printStackTrace();
+                for(ClientDetails client : clients) {
+                    if (client.getPhoneNo() == phoneNumber) {
+                        clients.remove(client);
+                    }
+                }
+                return;
+            }
+
+            //now server will check in the array list of clients for the given phone No and send message to hi
+            for(ClientDetails d : clients){
+                if(d.getPhoneNo().equals(data)){
+                    try {
+                        UserTable userTable = UserTable.getInstance();
+                        d.getOos().writeObject(phoneNumber);
+                        d.getOos().writeObject(t);
+                        d.getOos().writeObject(message);
+                        d.getOos().flush();
+                        break;
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        return;
+                    }
+                }
+            }
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        ServerSocket ss = null;
+        //Socket socket;
 
         try {
-            serverSocket=new ServerSocket(6963);
+            ss=new ServerSocket(6963);
             System.out.println("Server started");
         }
         catch (IOException e) {
             e.printStackTrace();
         }
-        while(true) {
-            try {
-                socket=serverSocket.accept();
-
-//                System.out.println("Client Connected");
-                ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
-                ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
-                String phoneNumber = ois.readObject().toString();
-                String password = ois.readObject().toString();
-                System.out.println("Client Connected");
-
-                //Authentication now
-                FullNameProfilePic fnpc = UserTable.getInstance().authentication(password,phoneNumber);
-                if(fnpc!=null)
-                    oos.writeObject(phoneNumber);
-                else
-                    oos.writeObject(null);
-
-                if(fnpc != null) {
-                    currentlyActiveUser.add(phoneNumber);
-                    HandleClientRequest clientHandler = new HandleClientRequest(fnpc.getFullName(),fnpc.getImage(),phoneNumber,socket,ois,oos);
-                    clientHandlers.add(clientHandler);
-                    Thread t = new Thread(clientHandler);
-                    t.start(); //starting new thread to handle client request
-                }
-            } catch (IOException | ClassNotFoundException e) {
-                e.printStackTrace();
-                
-            }
+        while (true) {
+            Socket s = ss.accept();
+            Server server = new Server(s);
+            Thread t = new Thread(server);
+            t.start();
         }
     }
+
     public static Connection getConnection()  {
         if (conn != null) return conn;
         try {
@@ -76,7 +131,7 @@ public class Server {
             return null;
         }
     }
-    private static Connection getConnection(String databaseName, String UserName, String password) {
+    /*private static Connection getConnection(String databaseName, String UserName, String password) {
         try {
             Class.forName("com.mysql.jdbc.Driver");
             conn = DriverManager.getConnection("jdbc:mysql://localhost/" + databaseName + "?user=" + UserName + "&password=" + password);
@@ -84,5 +139,5 @@ public class Server {
             e.printStackTrace();
         }
         return conn;
-    }
+    }*/
 }
