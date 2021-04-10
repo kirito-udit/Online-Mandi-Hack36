7c7f9ee117b1516acb81f175352d602c591a3a1f
Kotecha Udit Hitendra <64374551+kirito-udit@users.noreply.github.com>
2021-04-10 09:58:40 +0530

    Create invoice.java

---
+++ OnlineMandi/src/sample/invoice.java
@@ -0,0 +1,12 @@
+package sample;
+
+import javax.swing.*;
+
+public class invoice {
+    private JPanel panel1;
+    private JTextArea textArea1;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
