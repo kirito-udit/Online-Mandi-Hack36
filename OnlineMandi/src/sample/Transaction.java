81d9c9200fe4e182b9b5a110550c11fbf6693059
Kotecha Udit Hitendra <64374551+kirito-udit@users.noreply.github.com>
2021-04-10 13:10:49 +0530

    Create Transaction.java

---
+++ OnlineMandi/src/sample/Transaction.java
@@ -0,0 +1,69 @@
+package sample;
+
+import java.sql.Timestamp;
+
+public class Transaction {
+        public String transactionID;
+        public String sellerPhone;
+        public String buyerPhone;
+        public String cropName;
+        public int price;
+        public int quantity;
+        public Timestamp timestamp;
+
+    public String getTransactionID() {
+        return transactionID;
+    }
+
+    public void setTransactionID(String transactionID) {
+        this.transactionID = transactionID;
+    }
+
+    public String getSellerPhone() {
+        return sellerPhone;
+    }
+
+    public void setSellerPhone(String sellerPhone) {
+        this.sellerPhone = sellerPhone;
+    }
+
+    public String getBuyerPhone() {
+        return buyerPhone;
+    }
+
+    public void setBuyerPhone(String buyerPhone) {
+        this.buyerPhone = buyerPhone;
+    }
+
+    public String getCropName() {
+        return cropName;
+    }
+
+    public void setCropName(String cropName) {
+        this.cropName = cropName;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Timestamp timestamp) {
+        this.timestamp = timestamp;
+    }
+}
