45556ca6efc34992e41ccaf6a2fc753fb1d211f8
Kotecha Udit Hitendra <64374551+kirito-udit@users.noreply.github.com>
2021-04-10 10:07:00 +0530

    Update Distance.java

--- OnlineMandi/src/sample/Distance.java
+++ OnlineMandi/src/sample/Distance.java
@@ -27,14 +27,14 @@ public class Distance extends JFrame implements Runnable{
         setTitle("The Location of Your Destination");
         setVisible(true);
         setBounds(0,0,500, 500);
-        //    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         panel = new JFXPanel();
         add(panel);
         Platform.runLater(() -> {
             WebView view2 = new WebView();
-            //view.getEngine().load("https://www.mapquest.com/");
-            String address="https://wikimapia.org/#lang=en&lat=28.304683&lon=76.706200&z=12&m=w&gz=0;"+l2+";"+l1+";0;"+d2+";"+d1+";0";
-    //        String address = "https://www.openstreetmap.org/directions?engine=fossgis_osrm_car&route="+l1+"%2C"+l2+"%3B"+d1+"%2C"+d2+"#map=7/29.912/79.871";
+            view.getEngine().load("https://www.mapquest.com/");
+      //      String address="https://wikimapia.org/#lang=en&lat=28.304683&lon=76.706200&z=12&m=w&gz=0;"+l2+";"+l1+";0;"+d2+";"+d1+";0";
+            String address = "https://www.openstreetmap.org/directions?engine=fossgis_osrm_car&route="+l1+"%2C"+l2+"%3B"+d1+"%2C"+d2+"#map=7/29.912/79.871";
             view2.getEngine().load(address);
             panel.setScene(new Scene(view2));
         });
