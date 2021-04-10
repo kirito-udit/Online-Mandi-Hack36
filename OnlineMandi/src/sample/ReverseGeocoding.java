6da6355bb96def85f1d012d3f5f1e87864b76172
Kotecha Udit Hitendra <64374551+kirito-udit@users.noreply.github.com>
2021-04-10 14:42:15 +0530

    Create ReverseGeocoding.java

---
+++ OnlineMandi/src/sample/ReverseGeocoding.java
@@ -0,0 +1,24 @@
+import sys
+import geopy
+from geopy.geocoders import Nominatim
+from geopy.extra.rate_limiter import RateLimiter
+
+
+def my_test(lat, lon):
+    locator = Nominatim(user_agent='google')
+    coordinates = lat, lon
+    location = locator.reverse(coordinates)
+    print(location)
+    with open('C:\\Users\\hp\\Desktop\\OnlineMandi\\src\\sample\\Resources\\location.txt'
+              , 'w') as f:
+        f.write(str(location))
+    
+
+    # return location
+    # specify path or else it will be created where you run your java code
+
+    # with open("C:/Users/HP/Documents/OnlineMandi/src/sample/Resources/location.txt"
+    #           , 'w') as target:
+    #     target.write(location)
+
+my_test(sys.argv[1], sys.argv[2])
