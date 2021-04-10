59f7ee41f4d07759afbf7109d60e5c99516d508c
Saurabh Singh <60233633+mrdinosaurabh@users.noreply.github.com>
2021-04-10 12:55:30 +0530

    GeoIpify API key modified

--- OnlineMandi/src/sample/GeoipifyAPIQuery.java
+++ OnlineMandi/src/sample/GeoipifyAPIQuery.java
@@ -1,20 +1,25 @@
 package sample;
-import java.util.*;
-import java.lang.*;
-import java.io.*;
-import java.net.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+
 public class GeoipifyAPIQuery {
-    public static void main(String[]args){
-        String IP = "8.8.8.8";
+    public static void main(String[] args) throws IOException {
+        String IP;
         String API_KEY = "at_OlwZQUtRfGC83GyQBhrrA863ixHMS";
         String API_URL = "https://geo.ipify.org/api/v1?";
-        String url = API_URL + "&apiKey=" + API_KEY +
-                "&ipAddress=" + IP;
-        try (java.util.Scanner s =
-                     new java.util.Scanner(new java.net.URL(url).openStream())) {
+        URL url_name = new URL("http://bot.whatismyipaddress.com");
+
+        BufferedReader sc = new BufferedReader(new InputStreamReader(url_name.openStream()));
+        IP = sc.readLine().trim();
+        String url = API_URL + "&apiKey=" + API_KEY + "&ipAddress=" + IP;
+        try (java.util.Scanner s = new java.util.Scanner(new java.net.URL(url).openStream())) {
             System.out.println(s.useDelimiter("\\A").next());
         } catch (Exception ex) {
             ex.printStackTrace();
-        }
+        } //https://geo.ipify.org/api/v1?apiKey=at_OlwZQUtRfGC83GyQBhrrA863ixHMS&ipAddress=8.8.8.8
+
     }
 }
