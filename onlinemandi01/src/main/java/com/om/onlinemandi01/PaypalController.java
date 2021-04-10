90d93f846b039d89171cf4203fa955cd16cd73e1
kirito-udit <uditkotecha60@gmail.com>
2021-04-10 09:48:49 +0530

    Springboot-ject

---
+++ onlinemandi01/src/main/java/com/om/onlinemandi01/PaypalController.java
@@ -0,0 +1,73 @@
+package com.om.onlinemandi01;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.paypal.api.payments.Links;
+import com.paypal.api.payments.Payment;
+import com.paypal.base.rest.PayPalRESTException;
+import org.springframework.web.servlet.ModelAndView;
+import org.thymeleaf.context.Context;
+@Controller
+public class PaypalController {
+
+    @Autowired
+    PaypalService service;
+
+    public static final String SUCCESS_URL = "pay/success";
+    public static final String CANCEL_URL = "pay/cancel";
+
+//    @GetMapping("/")
+//    public String home() {
+//        return "home";
+//    }
+
+    @GetMapping("/")
+    public String payment(){//@ModelAttribute("order") Order order) {
+        try {
+           // Model model = new Model();
+       //     model.addAttribute("variableName", "someValue");
+//            Payment payment = service.createPayment(order.getPrice(), order.getCurrency(), order.getMethod(),
+//                    order.getIntent(), order.getDescription(), "http://localhost:9090/" + CANCEL_URL,
+//                    "http://localhost:9090/" + SUCCESS_URL);
+            Payment payment = service.createPayment(99.90, "INR", "paypal",
+                    "sale", "opop", "http://localhost:9090/" + CANCEL_URL,
+                    "http://localhost:9090/" + SUCCESS_URL);
+            for(Links link:payment.getLinks()) {
+                if(link.getRel().equals("approval_url")) {
+                    return "redirect:"+link.getHref();
+                }
+            }
+
+        } catch (PayPalRESTException e) {
+
+            e.printStackTrace();
+        }
+        return "redirect:/";
+    }
+
+    @GetMapping(value = CANCEL_URL)
+    public String cancelPay() {
+        return "cancel";
+    }
+
+    @GetMapping(value = SUCCESS_URL)
+    public String successPay(@RequestParam("paymentId") String paymentId, @RequestParam("PayerID") String payerId) {
+        try {
+            Payment payment = service.executePayment(paymentId, payerId);
+            System.out.println(payment.toJSON());
+            if (payment.getState().equals("approved")) {
+                return "success";
+            }
+        } catch (PayPalRESTException e) {
+            System.out.println(e.getMessage());
+        }
+        return "redirect:/";
+    }
+
+}
---
+++ onlinemandi01/src/test/java/com/om/onlinemandi01/Onlinemandi01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.om.onlinemandi01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Onlinemandi01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
