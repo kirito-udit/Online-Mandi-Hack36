0fdd19af9c703d807cecffbeea86142c8e1e606a
Saurabh Singh <60233633+mrdinosaurabh@users.noreply.github.com>
2021-04-10 13:48:42 +0530

    Fixed Bugs in chats

--- OnlineMandi/src/sample/ChatsController.java
+++ OnlineMandi/src/sample/ChatsController.java
@@ -160,3 +160,131 @@ public class ChatsController implements Initializable, Runnable{
         sendTextField.setText("");
     }
 }
+
+    public void setConvoList(ArrayList<Conversation> convoList) {
+        this.convoList = convoList;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+    @FXML
+    private TextField sendTextField;
+    @FXML
+    private TableView conversationTableView;
+    @FXML
+    private TextArea conversationTextArea;
+    @FXML
+    private Button sendButton;
+    @FXML
+    private TableColumn phoneNoTableColumn;
+    @FXML
+    private TableColumn nameTableColumn;
+    private ChatsController cpc;
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+    //    public ChatsController(ChatsController cpc){
+//        this.cpc = cpc;
+//    }
+    public void first(String name, String phoneNo, ChatsController cpc) throws InterruptedException {
+        this.name = name;
+        this.phoneNo = phoneNo;
+        this.cpc = cpc;
+        System.out.println(name);
+        System.out.println(phoneNo);
+        phoneNoTableColumn.setCellValueFactory(
+                new PropertyValueFactory<Conversation,String>("client")
+        );
+        nameTableColumn.setCellValueFactory(
+                new PropertyValueFactory<Conversation,String>("nameOfClient")
+        );
+        ArrayList<Conversation> list = MessageManager.getInstance().getAllConversations(phoneNo,name);
+
+        observableConvoList= FXCollections.observableList(list);
+        conversationTableView.setItems(observableConvoList);
+        conversationTableView.getSelectionModel().select(0);
+        setConvoTextArea();
+
+        conversationTextArea.textProperty().addListener((observableValue, s, t1) -> conversationTextArea.setScrollTop(Double.MAX_VALUE));
+
+        //now the thread for receiving messages will start separately
+        Thread t = new Thread(cpc);
+        t.start();
+
+    }
+    @Override
+    public  void run() {
+        while(true) {
+            try {
+
+                //if the current selected item's phone No is same as the phone No if the incoming message
+                //we simply display it on the text area
+                //otherwise we will send a message to the the sender that his message has not been seen yet
+                //and highlight that msg
+                String senderPhoneNo = (String) Main.ois.readObject();
+                Timestamp timestamp = (Timestamp) Main.ois.readObject();
+                String message = (String) Main.ois.readObject();
+                System.out.println(message);
+                UserTable userTable = UserTable.getInstance();
+                String senderName = userTable.getFullName(senderPhoneNo);
+                Conversation convo = (Conversation) this.conversationTableView.getSelectionModel().getSelectedItem();
+
+                if (convo.getClient().equals(senderPhoneNo)) {
+                    this.conversationTextArea.setText(this.conversationTextArea.getText() +"\n"+ senderName + "\n" + timestamp + "\n" + message);
+                    this.conversationTextArea.appendText("");
+                }
+                updateTable();
+            }catch(Exception e){
+                e.printStackTrace();
+                return;
+            }
+        }
+    }
+
+    public void updateTable(){
+        int index = conversationTableView.getSelectionModel().getSelectedIndex();
+        ArrayList<Conversation> list = MessageManager.getInstance().getAllConversations(this.phoneNo,this.name);
+        this.observableConvoList= FXCollections.observableList(list);
+        this.conversationTableView.setItems(observableConvoList);
+        this.conversationTableView.getSelectionModel().select(index);
+    }
+
+    @FXML
+    public void clickItem(MouseEvent event) {
+        updateTable();
+        setConvoTextArea();
+    }
+
+    public void setConvoTextArea() {
+        Conversation convo = (Conversation)this.conversationTableView.getSelectionModel().getSelectedItem();
+        this.conversationTextArea.setText(convo.getConvo());
+        this.conversationTextArea.appendText("");
+    }
+    @FXML
+    public void sendButtonResponse(ActionEvent e) throws IOException {
+        Conversation convo = (Conversation) conversationTableView.getSelectionModel().getSelectedItem();
+        Main.oos.writeObject(convo.getClient());
+        Main.oos.writeObject(new Timestamp(System.currentTimeMillis()));
+        Main.oos.writeObject(sendTextField.getText());
+        conversationTextArea.setText(conversationTextArea.getText()+
+                "\n"+name+"\n"+new Timestamp(System.currentTimeMillis())+
+                "\n"+sendTextField.getText());
+        conversationTextArea.appendText("");
+        Main.oos.flush();
+        sendTextField.setText("");
+    }
+}
