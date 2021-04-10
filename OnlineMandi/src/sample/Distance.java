f2669ab297b458c7aa461d216b90c01ad77ae181
Kotecha Udit Hitendra <64374551+kirito-udit@users.noreply.github.com>
2021-04-10 14:58:25 +0530

    Update Distance.java

--- OnlineMandi/src/sample/Distance.java
+++ OnlineMandi/src/sample/Distance.java
@@ -49,5 +49,32 @@ public class Distance extends JFrame implements Runnable{
 //       // SwingUtilities.invokeLater(new sample.WebBrowser());
 //        objd.run();
 //    }
+public static double getDistanceBetweenTwoPlaces(String lt1, String ln1,String lt2, String ln2) {
 
+        double lon1 = Double.parseDouble(ln1);
+        double lon2 = Double.parseDouble(ln2);
+        double lat1 = Double.parseDouble(lt1);
+        double lat2 = Double.parseDouble(lt2);
+
+        lon1 = Math.toRadians(lon1);
+        lon2 = Math.toRadians(lon2);
+        lat1 = Math.toRadians(lat1);
+        lat2 = Math.toRadians(lat2);
+
+        // Haversine formula
+        double dlon = lon2 - lon1;
+        double dlat = lat2 - lat1;
+        double a = Math.pow(Math.sin(dlat /2),2)
+                + Math.cos(lat1) * Math.cos(lat2)
+                * Math.pow(Math.sin(dlon /2),2);
+
+        double c =2 * Math.asin(Math.sqrt(a));
+
+        // Radius of earth in kilometers. Use 3956
+        // for miles
+        double r =6371;
+
+        // calculate the result
+        return(c * r);
+    }
 }
