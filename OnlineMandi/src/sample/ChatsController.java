78740a97d269038a6a68a0b40eadb51410768a97
Kotecha Udit Hitendra <64374551+kirito-udit@users.noreply.github.com>
2021-04-10 13:42:03 +0530

    Update ChatsController.java

--- OnlineMandi/src/sample/ChatsController.java
+++ OnlineMandi/src/sample/ChatsController.java
@@ -5,6 +5,8 @@ import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -62,6 +64,9 @@ public class ChatsController implements Initializable {
     @FXML
     private Conversation lastSelected;
 
+    @FXML
+    private TextField searchTextField;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
@@ -80,37 +85,79 @@ public class ChatsController implements Initializable {
                 new PropertyValueFactory<Conversation,String>("nameOfClient")
         );
         conversationTableView.setItems(observableConvoList);
-        conversationTableView.getSelectionModel().select(0);
+        if(lastSelected == null)
+                conversationTableView.getSelectionModel().select(0);
+        else
+                conversationTableView.getSelectionModel().select(lastSelected);
         lastSelected = (Conversation) conversationTableView.getSelectionModel().getSelectedItem();
         setConvoTextArea();
+
         conversationTableView.getSelectionModel().selectedItemProperty().addListener((obs, oldSelection, newSelection) -> {
-            int idx = conversationTableView.getSelectionModel().getSelectedIndex();
-            Platform.runLater(new Runnable() {
-                @Override
-                public void run() {
-                    ArrayList<Conversation> list2 = MessageManager.getInstance().getAllConversations(phoneNo,name);
-                    observableConvoList= FXCollections.observableList(list2);
-                }
-            });
-            phoneNoTableColumn.setCellValueFactory(
-                    new PropertyValueFactory<Conversation,String>("client")
-            );
-            nameTableColumn.setCellValueFactory(
-                    new PropertyValueFactory<Conversation,String>("nameOfClient")
-            );
-            conversationTableView.setItems(observableConvoList);
-            conversationTableView.getSelectionModel().select(idx);
+            lastSelected = (Conversation) newSelection;
             setConvoTextArea();
         });
+
+        phoneNoTableColumn.setCellValueFactory(
+                new PropertyValueFactory<Conversation,String>("client")
+        );
+        nameTableColumn.setCellValueFactory(
+                new PropertyValueFactory<Conversation,String>("nameOfClient")
+        );
+        conversationTableView.setItems(observableConvoList);
+        // conversationTableView.getSelectionModel().select(idx);
+        setConvoTextArea();
+//        });
+
+        // Wrap the ObservableList in a FilteredList (initially display all data).
+        FilteredList<Conversation> filteredData = new FilteredList<>(observableConvoList, b -> true);
+
+        // 2. Set the filter Predicate whenever the filter changes.
+        searchTextField.textProperty().addListener((observable, oldValue, newValue) -> {
+            filteredData.setPredicate(convo -> {
+                // If filter text is empty, display all persons.
+
+                if (newValue == null || newValue.isEmpty()) {
+                    return true;
+                }
+
+                // Compare first name and last name of every person with filter text.
+                String lowerCaseFilter = newValue.toLowerCase();
+
+                if (convo.getNameOfClient().toLowerCase().indexOf(lowerCaseFilter) != -1 ) {
+                    return true; // Filter matches first name.
+                } else if (convo.getClient().toLowerCase().indexOf(lowerCaseFilter) != -1) {
+                    return true; // Filter matches last name.
+                }
+                else
+                    return false; // Does not match.
+            });
+        });
+
+        // 3. Wrap the FilteredList in a SortedList.
+        SortedList<Conversation> sortedData = new SortedList<>(filteredData);
+
+        // 4. Bind the SortedList comparator to the TableView comparator.
+        // 	  Otherwise, sorting the TableView would have no effect.
+        sortedData.comparatorProperty().bind(conversationTableView.comparatorProperty());
+
+        // 5. Add sorted (and filtered) data to the table.
+        conversationTableView.setItems(sortedData);
+
+
     }
-//    @FXML
-//    public void clickItem(MouseEvent event) {
-//        setConvoTextArea();
-//    }
+
     @FXML
     public void setConvoTextArea() {
         Conversation convo = (Conversation) conversationTableView.getSelectionModel().getSelectedItem();
-        conversationTextArea.setText(convo.getConvo()+"\n");
+        if(convo == null) {
+            convo = lastSelected;
+            conversationTableView.getSelectionModel().select(lastSelected);
+        }
+        try {
+            conversationTextArea.setText(convo.getConvo());
+        } catch(Exception e) {
+
+        }
     }
     @FXML
     public void sendButtonResponse(ActionEvent e) {
