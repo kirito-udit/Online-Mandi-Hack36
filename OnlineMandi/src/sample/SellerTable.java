626aee544f60c9f36248771e40bad8975df5fc6b
Kotecha Udit Hitendra <64374551+kirito-udit@users.noreply.github.com>
2021-04-10 13:09:54 +0530

    Update SellerTable.java

--- OnlineMandi/src/sample/SellerTable.java
+++ OnlineMandi/src/sample/SellerTable.java
@@ -12,22 +12,24 @@ public class SellerTable {
     public static final String CONNECTION_STRING = "jdbc:sqlite:"+dbFile.getAbsolutePath()+"\\"+DB_NAME;
     public static final String SELL_TABLE = "SellTable";
     public static final String COLUMN_SELLER_PHONE = "SellerPhone";
-    public static final String COLUMN_SELLER_NAME = "SellerName";
     public static final String COLUMN_START_DATE = "StartDate";
     public static final String COLUMN_CROP_NAME = "CropName";
     public static final String COLUMN_QUANTITY = "Quantity";
     public static final String COLUMN_END_DATE = "EndDate";
     public static final String COLUMN_OFFER_ID = "OfferId";
     public static final String COLUMN_PRICE = "Price";
-
+    public static final String COLUMN_DESCRIPTION="Description";
     public static final String ADD_OFFER = " INSERT INTO " + SELL_TABLE + " ( "+COLUMN_CROP_NAME+" , "+COLUMN_QUANTITY+" , "+
-            COLUMN_PRICE+" , "+COLUMN_START_DATE+" , "+COLUMN_END_DATE+" , "+COLUMN_SELLER_NAME+" , "+COLUMN_SELLER_PHONE+" ) VALUES ( ? , ? , ? , ? , ? , ? , ? ) ";
+            COLUMN_PRICE+" , "+COLUMN_START_DATE+" , "+COLUMN_END_DATE+" , "+COLUMN_SELLER_PHONE+" , "+COLUMN_DESCRIPTION+" ) VALUES ( ? , ? , ? , ? , ? , ? , ? ) ";
     public static final String UPDATE_OFFER_PRICE = " UPDATE " + SELL_TABLE + " SET " + COLUMN_PRICE + " = ? WHERE " +
             COLUMN_OFFER_ID + " = ? ";
     public static final String UPDATE_OFFER_END_DATE = " UPDATE " + SELL_TABLE + " SET " + COLUMN_END_DATE + " = ? " + " WHERE " +
             COLUMN_OFFER_ID + " = ? ";
     public static final String UPDATE_OFFER_QUANTITY = " UPDATE " + SELL_TABLE + " SET " + COLUMN_QUANTITY + " = ? " + " WHERE " +
             COLUMN_OFFER_ID + " = ? ";
+    public static final String UPDATE_DESCRIPTION=" UPDATE "+SELL_TABLE+" SET "+COLUMN_DESCRIPTION+" = ? "+" WHERE "+
+            COLUMN_OFFER_ID+" = ? ";
+
     public static final String DELETE_OFFER = " DELETE FROM " + SELL_TABLE + " WHERE " + COLUMN_OFFER_ID + " = ? ";
     public static final String GET_ALL_OFFERS = " SELECT * FROM " + SELL_TABLE;
 
@@ -36,6 +38,7 @@ public class SellerTable {
     public PreparedStatement updateOfferPrice;
     public PreparedStatement updateOfferEndDate;
     public PreparedStatement updateOfferQuantity;
+    public PreparedStatement updateDescription;
     public PreparedStatement deleteOffer;
     public PreparedStatement getAllOffers;
 
@@ -72,7 +75,8 @@ public class SellerTable {
     }
 
     public boolean addOffer(String cropName, int quantity, int price,
-                            Date startDate, Date endDate, String sellerName, String sellerPhone) {
+                            Date startDate, Date endDate,
+                            String sellerPhone,String description) {
         try {
             conn = DriverManager.getConnection(CONNECTION_STRING);
             addOffer=conn.prepareStatement(ADD_OFFER);
@@ -81,8 +85,8 @@ public class SellerTable {
             addOffer.setInt(3, price);
             addOffer.setDate(4, startDate);
             addOffer.setDate(5, endDate);
-            addOffer.setString(6, sellerName);
-            addOffer.setString(7, sellerPhone);
+            addOffer.setString(6, sellerPhone);
+            addOffer.setString(7,description);
             addOffer.executeUpdate();
             return true;
         } catch (SQLException e) {
@@ -169,6 +173,29 @@ public class SellerTable {
         }
     }
 
+    public boolean updateDescription(int offerId,String description){
+        try {
+            conn = DriverManager.getConnection(CONNECTION_STRING);
+            updateDescription=conn.prepareStatement(UPDATE_DESCRIPTION);
+            updateOfferPrice.setString(1,description);
+            updateOfferPrice.setInt(2,offerId);
+            int affectedRows=updateDescription.executeUpdate();
+            if(affectedRows!=1){
+                throw new SQLException("Couldn't update the offer description");
+            }
+            return true;
+        }catch (SQLException e){
+            System.out.println("Error occured while updating the offer description "+e.getMessage());
+            return false;
+        }finally {
+            try{
+                updateDescription.close();
+                conn.close();
+            }catch (SQLException e){
+                System.out.println("Error occured while closing updateDescription "+e.getMessage());
+            }
+        }
+    }
     //this function returns the list of all the offers of a particular seller
     public ArrayList<Offer> getAllOffers() {
         try {
@@ -177,7 +204,9 @@ public class SellerTable {
             ArrayList <Offer> offers = new ArrayList<Offer>();
             ResultSet resultSet = getAllOffers.executeQuery();
             while (resultSet.next()) {
-                Offer offer = new Offer(resultSet.getInt(1), resultSet.getString(2), resultSet.getInt(3), resultSet.getInt(4), resultSet.getDate(5), resultSet.getDate(6), resultSet.getString(7), resultSet.getString(8));
+                Offer offer = new Offer(resultSet.getInt(1), resultSet.getString(2), resultSet.getInt(3),
+                        resultSet.getInt(4), resultSet.getDate(5), resultSet.getDate(6),
+                        resultSet.getString(7),resultSet.getString(8));
                 System.out.println(offer.getOfferId());
                 System.out.println(offer.getStartDate());
                 System.out.println(offer.getEndDate());
