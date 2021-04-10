0b721124609e67d62285d45e9d79dea46025f46d
Saurabh Singh <60233633+mrdinosaurabh@users.noreply.github.com>
2021-04-10 10:05:43 +0530

    Update Distance.java

--- OnlineMandi/src/sample/Distance.java
+++ OnlineMandi/src/sample/Distance.java
@@ -23,7 +23,22 @@ public class Distance extends JFrame implements Runnable{
         d1 = a;
         d2 = b;
     }
-  
+    public void run() {
+        setTitle("The Location of Your Destination");
+        setVisible(true);
+        setBounds(0,0,500, 500);
+        //    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        panel = new JFXPanel();
+        add(panel);
+        Platform.runLater(() -> {
+            WebView view2 = new WebView();
+            //view.getEngine().load("https://www.mapquest.com/");
+            String address="https://wikimapia.org/#lang=en&lat=28.304683&lon=76.706200&z=12&m=w&gz=0;"+l2+";"+l1+";0;"+d2+";"+d1+";0";
+    //        String address = "https://www.openstreetmap.org/directions?engine=fossgis_osrm_car&route="+l1+"%2C"+l2+"%3B"+d1+"%2C"+d2+"#map=7/29.912/79.871";
+            view2.getEngine().load(address);
+            panel.setScene(new Scene(view2));
+        });
+    }
 
     public static void main(String[] args) {
         Distance objd  = new Distance();
